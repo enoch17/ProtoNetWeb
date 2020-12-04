e8b59475e460504eeca9fa60136b0d6c044314e6
enoch17 <enochomolere1@gmail.com>
2020-12-04 21:27:11 +0100

    Correction

--- pages/admin/dashboard.php
+++ pages/admin/dashboard.php
@@ -2,12 +2,15 @@
 session_start();
 require "header.php";
 require "sidebar.php";?>
+<!-- Html goes here -->
 <div class="btn-group">
   <button>New Users</button>
   <button>Active Users</button>
   <button>Inactive Users</button>
   <button>Total Data Usage</button>
 </div>
+
+<!-- html ends here -->
 <?
 require "footer.php";
 ?>
